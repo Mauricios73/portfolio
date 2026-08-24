a8d1866bf558436a3eaecd9a426afb793393f704
Mauricio Portela <137836037+Mauricios73@users.noreply.github.com>
2026-08-24 16:00:55 -0300

    feat: rebuild portfolio home around real projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,26 @@
-// import Image from 'next/image'
 import { Experience } from './components/experience/experience'
 import { Header } from './components/header/header'
 import { Info } from './components/information/information'
+import { Projects } from './components/projects/projects'
 import { EmailIcon } from './components/icons/email-icon'
 import { SocialBtns } from './components/social-btn/social-btn'
-import "./styles/home.scss"
+import './styles/home.scss'
 
 export default function Home() {
   return (
-    <main className='container'>
-      <Header/>
-      <Experience/>
+    <main className="container">
+      <Header />
+      <section className="intro">
+        <span className="eyebrow">CYBERSECURITY / WIRELESS / EMBEDDED</span>
+        <h1>Construindo sistemas, explorando redes e aprendendo segurança.</h1>
+        <p>Meu foco atual está em segurança cibernética, redes Wi-Fi e sistemas embarcados. Gosto de transformar ideias em projetos reais, principalmente com ESP32, C/C++ e tecnologias web.</p>
+      </section>
+      <Experience />
+      <Projects />
       <Info />
-
       <div className="buttons">
-      <SocialBtns />
-
-        <a className='primary-btn' href='mailto:mauricioportela73@gmail.com'>
-          contato!
-          <EmailIcon/>
-        </a>
+        <SocialBtns />
+        <a className="primary-btn" href="mailto:mauricioportela73@gmail.com">Contato <EmailIcon /></a>
       </div>
     </main>
   )
